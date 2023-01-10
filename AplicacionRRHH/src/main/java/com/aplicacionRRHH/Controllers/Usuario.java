eb767d08e0e897704a03ec922ed6cc5c58b92a15
Usuario <Usuario@DESKTOP-AR817US>
2023-01-10 13:59:27 +0100

    Primeros pasos conexion bbdd

--- AplicacionRRHH/src/main/java/com/aplicacionRRHH/Controllers/Usuario.java
+++ AplicacionRRHH/src/main/java/com/aplicacionRRHH/Controllers/Usuario.java
@@ -1,7 +1,13 @@
 package com.aplicacionRRHH.Controllers;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="usuario")
 public class Usuario {
 
+	Long id;
 	String nombre;
 	String rol;
 }
