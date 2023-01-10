7f93c13ba5bba42ce308d0cfae59fd9aa31aef64
Usuario <Usuario@DESKTOP-AR817US>
2023-01-10 13:39:31 +0100

    prueba Luis

--- AplicacionRRHH/src/main/java/com/aplicacionRRHH/Controllers/InicioController.java
+++ AplicacionRRHH/src/main/java/com/aplicacionRRHH/Controllers/InicioController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class InicioController {
+	//prueba Luis
 
 	@GetMapping("/inicio")
 	public String inicio(Model model){
