7ad52e8d6db92c201b57b68605c60f580009f6ce
usuario <usuario@LAPTOP-O9A44FQJ>
2023-01-18 10:43:22 +0100

    Cambios 18 enero  por la mañana

---
+++ AplicacionRRHH/src/main/java/com/aplicacionRRHH/Controllers/ParametrosController.java
@@ -0,0 +1,62 @@
+package com.aplicacionRRHH.Controllers;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.aplicacionRRHH.Dao.CandidatoDao;
+import com.aplicacionRRHH.Dao.LocalidadDao;
+import com.aplicacionRRHH.modelos.Candidato;
+import com.aplicacionRRHH.modelos.Convocatoria;
+
+import jakarta.validation.Valid;
+
+@Controller
+public class ParametrosController {
+
+	@Autowired
+	private CandidatoDao daoCandidato;
+	
+	@Autowired
+	private LocalidadDao daoLocalidad;
+	
+	
+	@GetMapping("/configParametros")
+	public String inicio(Model model){
+
+		//model.addAttribute("candidatos", daoCandidato.findCandidato());
+		return "ConfigParametros";
+	}
+	
+	
+	@GetMapping("/nuevo2")
+	public String nuevoCandidato(Map<String, Object> model){
+
+		Candidato candidato = new Candidato();
+		candidato.setLocalidad(daoLocalidad.findOne(3L));
+		model.put("candidato", candidato);
+		model.put("localidades", daoLocalidad.findLocalidad());
+		return "NuevoCandidato";
+	}
+	
+	@PostMapping("/nuevo2")
+	public String crearConvocatoria(Candidato candidato) {
+
+		/*
+		if(result.hasErrors()) {
+			return "NuevoCandidato";
+		}
+		*/
+
+		candidato.setLocalidad(daoLocalidad.findOne(3L));
+		daoCandidato.save(candidato);
+		return "redirect:/candidatos/todos";
+	}
+}
